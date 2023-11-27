5d8e169e891dafdd2902abcdb1e587e07760b3c2
TC-Head <295243105@qq.com>
2023-11-27 20:59:58 +0800

    第四次版本提交,已经可以将项目的路径添加到环境变量中,直接在任意一个位置启动程序了,并且打包了nuitka的打包命令,以备不时之需

---
+++ Main.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'Main.exe' with Python DLL being found.
+set PATH=d:\application\python3.10.10-64;%PATH%
+set PYTHONHOME=D:\Application\Python3.10.10-64
+"%~dp0Main.exe" %*
